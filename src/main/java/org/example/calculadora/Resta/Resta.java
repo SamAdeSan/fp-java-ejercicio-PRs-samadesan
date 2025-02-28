8a02db2def01ede1a8d619ab431a39887477b6db
Jordi Porcar Muñoz <jorpormun@alu.edu.gva.es>
2025-02-28 08:54:51 +0100

    resta

---
+++ src/main/java/org/example/calculadora/Resta/Resta.java
@@ -0,0 +1,17 @@
+package org.example.calculadora.Resta;
+
+/**
+ * Clase que proporciona una operación de resta.
+ */
+public class Resta {
+
+    /**
+     * Resta dos números enteros.
+     * @param a Primer número
+     * @param b Segundo número
+     * @return El resultado de restar b a a
+     */
+    public static int restar(int a, int b) {
+        return a - b;
+    }
+}
