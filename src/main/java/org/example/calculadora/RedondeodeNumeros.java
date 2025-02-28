dd8c8895303f4233cf7fc05011c9d0571904882a
Abelsabater <abesabmun@alu.edu.gva.es>
2025-02-28 09:36:43 +0100

    Redondear numeros Abel Sabater

---
+++ src/main/java/org/example/calculadora/RedondeodeNumeros.java
@@ -0,0 +1,24 @@
+package org.example.calculadora;
+
+public class RedondeodeNumeros {
+    private double num;
+
+    public RedondeodeNumeros(double num) {
+        this.num = num;
+    }
+
+    public double getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+    public int numerordondejat(){
+        int numr = (int) this.num;
+        if(this.num-numr>=0.5){
+            numr+=1;
+        }
+        return numr;
+    }
+}
